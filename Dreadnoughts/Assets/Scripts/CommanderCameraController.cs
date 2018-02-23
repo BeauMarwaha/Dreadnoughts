53fea327545b7d8907d36773e10869b7563c5671
Beau Marwaha <beaumarwaha@gmail.com>
2018-02-22 22:57:19 -0500

    Camera Update
    Added a commander camera controller that handles rotating the top camera with the mouse and a UI that signifies each camera type.

---
+++ Dreadnoughts/Assets/Scripts/CommanderCameraController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Author: Beau Marwaha
+/// Camera Controller for the commander's camera.
+/// </summary>
+public class CommanderCameraController : MonoBehaviour {
+
+    // attributes
+    public float speed = 5.0f;
+    public Transform parentTransform;
+
+    // Use this for initialization
+    void Start ()
+    {
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        // Have the camera follow the mouse x axis
+        transform.RotateAround(parentTransform.position, Vector3.up, Input.GetAxis("Mouse X") * speed);
+    }
+}
