3eef659478b2408e769ebe1de3faedf01ab73012
Kritapas Nimtrakul <kxn9945@rit.edu>
2018-02-24 00:05:17 -0500

    Bullet

---
+++ Dreadnoughts/Assets/Scripts/Bullet.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+	private Vector3 nVector;
+
+	void DestroyObjectDelayed()
+	{
+		Destroy (this.gameObject, 5);
+	}
+
+	void OnCollisionEnter(Collision col)
+	{
+		if (col.gameObject.tag == "Enemy")
+		{
+			Destroy (this.gameObject);
+		}
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		nVector = Vector3.Normalize (this.transform.position);
+		nVector.y = 0.0f;
+		this.transform.position += (nVector*0.1f);
+		DestroyObjectDelayed ();
+	}
+}
---
+++ Dreadnoughts/Assets/Scripts/Shoot.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour {
+
+	public GameObject prefabBullet;
+	public GameObject tank;
+
+	void ShootBullet() {
+		Instantiate(prefabBullet,(tank.transform.position + new Vector3(0.0f,0.273f,0.0f) + (tank.transform.forward*0.59f)), Quaternion.identity);
+	}
+
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			ShootBullet();
+		}
+	}
+}
