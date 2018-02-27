2f5b7fe6013513ca6a3b99cf15a6a850a974528c
Beau Marwaha <beaumarwaha@gmail.com>
2018-02-26 22:25:04 -0500

    Added enemies and a win condition
    Added enemies and a win condition, also started on getting bullet shooting working

--- Dreadnoughts/Assets/Scripts/Shoot.cs
+++ Dreadnoughts/Assets/Scripts/Shoot.cs
@@ -6,12 +6,12 @@ public class Shoot : MonoBehaviour {
 
 	public GameObject prefabBullet;
 	public Transform tank;
+    public Transform turrent;
     public Transform bulletContainer;
 
 	void ShootBullet() {
-        Transform turretTransform = tank.GetChild(1);
-        Vector3 intialPosition = tank.position + new Vector3(0f, 0.273f, 0f) + turretTransform.forward * 0.59f;
-		Instantiate(prefabBullet, intialPosition, turretTransform.rotation, bulletContainer);
+        Vector3 intialPosition = tank.position + new Vector3(0f, 0.273f, 0f) + turrent.forward * 0.59f;
+		Instantiate(prefabBullet, intialPosition, turrent.rotation, bulletContainer);
 	}
 
 	void Update () {
---
+++ Dreadnoughts/Assets/Scripts/WinHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Author: Beau Marwaha
+/// Handles the game win condition.
+/// </summary>
+public class WinHandler : MonoBehaviour {
+
+    public GameObject winScreen;
+
+	// Update is called once per frame
+	void Update () {
+        // Retrieve all child transforms
+        Transform[] children = GetComponentsInChildren<Transform>();
+
+        // If there are no remaining child transforms all enemies have been destroyed and the game is over
+        if(children.Length == 1)
+        {
+            // SHow the win screen
+            winScreen.SetActive(true);
+        }
+	}
+}
