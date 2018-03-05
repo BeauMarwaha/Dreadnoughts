d58416eed2a0b277ff2f4cdbdf238a5b7ee772f9
Marie Hoff <marieofthemoon@gmail.com>
2018-03-04 20:57:13 -0500

    -Changed game level name to "Level1" -Added a basic health script for the player tank -Added some textures -Added GameOver and MainMenu scenes -MainMenu is very basic, but does have a working start game and quit game button.

---
+++ Dreadnoughts/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour {
+
+    public int startingHealth = 100;
+    public int currentHealth;
+
+    bool ishurt;
+    bool isalive;
+
+	// Use this for initialization
+	void Start () {
+        currentHealth = startingHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (ishurt == true)
+        {
+            Debug.Log("Ouch");
+        }
+        else
+        {
+
+        }
+
+        ishurt = false;
+        
+    }
+
+    void TakeDamage(int damageTaken)
+    {
+        ishurt = true;
+        currentHealth -= damageTaken;
+
+        if (currentHealth <= 0 && isalive == true)
+        {
+            KillPlayer();
+        }
+    }
+
+    void KillPlayer()
+    {
+        isalive = false;
+        SceneManager.LoadScene(2);
+        //The number refers to the scene in the build index which can change if we add more levels. Hard coding it for now to make it simple, can/should change later
+    }
+}
---
+++ Dreadnoughts/Assets/Scripts/UI Scripts/Quit_BTN.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Quit_BTN : MonoBehaviour {
+
+    public void QuittheGame()
+    {
+        Application.Quit();
+        print("GameQuit");
+        Debug.Log("You have clicked the button!");
+    }
+	
+}
