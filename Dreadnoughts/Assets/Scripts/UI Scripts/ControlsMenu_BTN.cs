217ab36f05af16cd0548f316a749defdf96a84ba
Beau Marwaha <beaumarwaha@gmail.com>
2018-03-23 11:03:36 -0400

    Added Controls Menu
    Added Controls Menu

---
+++ Dreadnoughts/Assets/Scripts/UI Scripts/ControlsMenu_BTN.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ControlsMenu_BTN : MonoBehaviour {
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("ControlsMenu");
+    }
+}
