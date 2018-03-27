2e77064018081df840e55743840bf42ab7024558
Beau Marwaha <beaumarwaha@gmail.com>
2018-03-27 17:34:38 -0400

    Bug Fixes
    Fixed heading bars issue, changed controls menu game title, added objective text to the main menu.

--- Dreadnoughts/Assets/Scripts/NumberHeader.cs
+++ Dreadnoughts/Assets/Scripts/NumberHeader.cs
@@ -10,7 +10,6 @@ public class NumberHeader : MonoBehaviour {
 	public float start = 526.0f;
 	// Use this for initialization
 	void Start () {
-        turret = GameObject.Find("Dreadnought");
 		for(int x = 360 ; x < 365; x++){
 
 			mText.text += x%366 + " | ";
