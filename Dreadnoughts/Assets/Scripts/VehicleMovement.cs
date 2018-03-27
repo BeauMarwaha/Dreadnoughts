23c5a373b573a9bda462ee436879d676695048b2
Conner Catanese <connerlcatanese@gmail.com>
2018-03-26 23:52:33 -0400

    add momentum-based movement and rotation

--- Dreadnoughts/Assets/Scripts/VehicleMovement.cs
+++ Dreadnoughts/Assets/Scripts/VehicleMovement.cs
@@ -10,25 +10,34 @@ public class VehicleMovement : MonoBehaviour
 {
 
     //attributes
-    private Vector3 vehiclePos;
-    private Vector3 velocity;
-    private Vector3 direction;
+    private Vector3 vehiclePosition;
+    private Vector3 vehicleLinearVelocity;
+    private Vector3 vehicleLinearAcceleration;
+
+    private Vector3 vehicleDirection;
+    private float vehicleAngularAcceleration;
+    private float vehicleAngularVelocity;
+
     private Vector3 turretDirection;
-    private Vector3 acceleration;
+    private float turretAngularAcceleration;
+    private float turretAngularVelocity;
 
     private Transform turretTransform;
 
     public float angleOfRotation;
     public float turretAngle;
-    public float maxSpeed;
-    public float accelRate;
+    public float maxVehicleLinearSpeed;
+    public float maxVehicleAngularSpeed;
+    public float maxTurretAngularSpeed;
 
     // Use this for initialization
     void Start()
     {
         //set initial values
         ResetPosition();
-        maxSpeed = 2.5f;
+        maxVehicleLinearSpeed = 2.5f;
+        maxVehicleAngularSpeed = 2.5f;
+        maxTurretAngularSpeed = 2.5f;
         turretTransform = transform.GetChild(1);
     }
 
@@ -48,41 +57,38 @@ public class VehicleMovement : MonoBehaviour
     /// </summary>
     void Drive()
     {
+        float tempAccelRate = 0;
+
         //if the user presses the I key accelerate the vehicle
         if (Input.GetKey(KeyCode.I))
         {
-            accelRate = 5f;
+            tempAccelRate += 5f;
         }
-        else if (Input.GetKey(KeyCode.K))
+
+        //if the user presses the K key decelerate the vehicle
+        if (Input.GetKey(KeyCode.K))
         {
-            //if the user presses the K key decelerate the vehicle
-            accelRate = -5f;
+            tempAccelRate -= 5f;
         }
-        else
-        { 
-            //if the user does not press either key stop acceleration and decelerate the vehicle back to a standstill
-            //stop acceleration
-            accelRate = 0;
 
-            //slow down the vehicle
-            velocity = Vector3.ClampMagnitude(velocity, velocity.magnitude * .8f);
-
-            //bring the car to a standstill after reaching a very small current speed
-            if (velocity.magnitude <= .000001)
-            {
-                velocity = Vector3.zero;
-            }
-        }
+        //decelerate the vehicle, providing natural soft-cap on speed
+        tempAccelRate += vehicleLinearVelocity.magnitude * -1.5f;
 
         //accelRate * direction = accel vector
-        acceleration = accelRate * direction.normalized * Time.deltaTime;
+        vehicleLinearAcceleration = tempAccelRate * vehicleDirection.normalized * Time.deltaTime;
         //add accel to vel
-        velocity += acceleration;
+        vehicleLinearVelocity += vehicleLinearAcceleration;
         //limit vel
-        velocity = Vector3.ClampMagnitude(velocity, maxSpeed);
+        vehicleLinearVelocity = Vector3.Project(Vector3.ClampMagnitude(vehicleLinearVelocity, maxVehicleLinearSpeed), vehicleDirection.normalized);
+
+        //bring the car to a standstill after reaching a very small current speed
+        if (vehicleLinearVelocity.magnitude <= 0.01f)
+        {
+            vehicleLinearVelocity = Vector3.zero;
+        }
 
         //add velocity to position
-        vehiclePos += velocity * Time.deltaTime;
+        vehiclePosition += vehicleLinearVelocity * Time.deltaTime;
     }
 
     /// <summary>
@@ -90,23 +96,44 @@ public class VehicleMovement : MonoBehaviour
     /// </summary>
     void Rotate()
     {
-        //if J key is pressed rotate to the left 2 deg
+        float tempRotAccelRate = 0;
+
+        //if J key is pressed rotate to the left
         if (Input.GetKey(KeyCode.J))
         {
-            direction = Quaternion.Euler(0, -2, 0) * direction;
-            turretDirection = Quaternion.Euler(0, -2, 0) * turretDirection;
-            angleOfRotation -= 2f;
-            turretAngle -= 2f;
+            tempRotAccelRate -= 5f;
         }
 
-        //if L key is pressed rotate to the right 2 deg
+        //if L key is pressed rotate to the right
         if (Input.GetKey(KeyCode.L))
         {
-            direction = Quaternion.Euler(0, 2, 0) * direction;
-            turretDirection = Quaternion.Euler(0, 2, 0) * turretDirection;
-            angleOfRotation += 2f;
-            turretAngle += 2f;
+            tempRotAccelRate += 5f;
         }
+
+        //provide natural friction and soft-cap on rotational velocity
+        tempRotAccelRate += vehicleAngularVelocity * -5f;
+
+        //accelRate * time = accel scalar
+        vehicleAngularAcceleration = tempRotAccelRate * Time.deltaTime;
+
+        //add accel to vel
+        vehicleAngularVelocity += vehicleAngularAcceleration;
+
+        //limit angular vel
+        vehicleAngularVelocity = Mathf.Min(Mathf.Max(vehicleAngularVelocity, -maxVehicleAngularSpeed), maxVehicleAngularSpeed);
+
+        //bring the car to a rotational standstill after reaching a very small current speed
+        if (Mathf.Abs(vehicleAngularVelocity) <= 0.01f)
+        {
+            vehicleAngularVelocity = 0f;
+        }
+
+        //add velocity to position
+        Quaternion rotationStrength = Quaternion.Euler(0, vehicleAngularVelocity, 0);
+        vehicleDirection = rotationStrength * vehicleDirection;
+        turretDirection = rotationStrength * turretDirection;
+        angleOfRotation += vehicleAngularVelocity;
+        turretAngle += vehicleAngularVelocity;
     }
 
     /// <summary>
@@ -114,19 +141,42 @@ public class VehicleMovement : MonoBehaviour
     /// </summary>
     void RotateTurret()
     {
-        //if A key is pressed rotate to the left 1 deg
+        float tempRotAccelRate = 0;
+
+        //if J key is pressed rotate to the left
         if (Input.GetKey(KeyCode.A))
         {
-            turretDirection = Quaternion.Euler(0, -1, 0) * turretDirection;
-            turretAngle -= 1f;
+            tempRotAccelRate -= 10f;
         }
 
-        //if D key is pressed rotate to the right 1 deg
+        //if L key is pressed rotate to the right
         if (Input.GetKey(KeyCode.D))
         {
-            turretDirection = Quaternion.Euler(0, 1, 0) * turretDirection;
-            turretAngle += 1f;
+            tempRotAccelRate += 10f;
         }
+
+        //provide natural friction and soft-cap on rotational velocity
+        tempRotAccelRate += turretAngularVelocity * -8f;
+
+        //accelRate * time = accel scalar
+        turretAngularAcceleration = tempRotAccelRate * Time.deltaTime;
+
+        //add accel to vel
+        turretAngularVelocity += turretAngularAcceleration;
+
+        //limit angular vel
+        turretAngularVelocity = Mathf.Min(Mathf.Max(turretAngularVelocity, -maxTurretAngularSpeed), maxTurretAngularSpeed);
+
+        //bring the car to a rotational standstill after reaching a very small current speed
+        if (Mathf.Abs(turretAngularVelocity) <= 0.03f)
+        {
+            turretAngularVelocity = 0f;
+        }
+
+        //add velocity to position
+        Quaternion rotationStrength = Quaternion.Euler(0, turretAngularVelocity, 0);
+        turretDirection = rotationStrength * turretDirection;
+        turretAngle += turretAngularVelocity;
     }
 
     /// <summary>
@@ -144,7 +194,7 @@ public class VehicleMovement : MonoBehaviour
     /// <returns>The position.</returns>
     public Vector3 GetPosition()
     {
-        return vehiclePos;
+        return vehiclePosition;
     }
 
     /// <summary>
@@ -152,12 +202,11 @@ public class VehicleMovement : MonoBehaviour
     /// </summary>
     public void ResetPosition()
     {
-        vehiclePos = new Vector3(0, 0.16f, 0); // Custom start position
-        velocity = Vector3.zero;
-        direction = Vector3.forward;
+        vehiclePosition = new Vector3(0, 0.16f, 0); // Custom start position
+        vehicleLinearVelocity = Vector3.zero;
+        vehicleDirection = Vector3.forward;
         turretDirection = Vector3.forward;
         angleOfRotation = 0;
-        accelRate = 0;
     }
 
     /// <summary>
@@ -166,7 +215,7 @@ public class VehicleMovement : MonoBehaviour
     void SetTransform()
     {
         //"Draw" the vehicle at its calculated position 
-        transform.position = vehiclePos;
+        transform.position = vehiclePosition;
 
         //rotate the vehicle to its calculated rotation
         transform.rotation = Quaternion.Euler(0, angleOfRotation, 0);
